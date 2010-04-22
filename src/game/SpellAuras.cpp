05aa7bdd9996ee5513a33c4eec7bd2edc5ed826f
pasdVn <pasdVn3@gmx.de>
2010-04-23 01:24:36 +0200

    [OV20] Fixed mana reg of 60069.

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -5543,6 +5543,9 @@ void Aura::HandleAuraModTotalEnergyPercentRegen(bool apply, bool /*Real*/)
     if(m_modifier.periodictime == 0)
         m_modifier.periodictime = 1000;
 
+    if(GetSpellProto()->Id == 60069)            // Dispersion HACK
+        m_modifier.m_miscvalue = 0;
+
     m_periodicTimer = m_modifier.periodictime;
     m_isPeriodic = apply;
 }
