c7366466260c81d52a0863f7577113d631166192
pasdVn <pasdVn3@gmx.de>
2010-06-09 00:38:52 +0200

    Converted some sql's back to ANSI :-/
    + typo

--- sql/custom/075_01_mangos_gossip_menu_option(OV59).sql
+++ sql/custom/075_01_mangos_gossip_menu_option(OV59).sql
@@ -1,3 +1,3 @@
-﻿DELETE FROM `gossip_menu_option` WHERE `menu_id` = 0 AND `id` = 15 OR `menu_id` = 4783 AND `id` = 0;
+DELETE FROM `gossip_menu_option` WHERE `menu_id` = 0 AND `id` = 15 OR `menu_id` = 4783 AND `id` = 0;
 INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `cond_1`, `cond_1_val_1`, `cond_1_val_2`, `cond_2`, `cond_2_val_1`, `cond_2_val_2`, `cond_3`, `cond_3_val_1`, `cond_3_val_2`) VALUES
-(4783, 0, 0, 'Ich möchte die Talente meines Beglieters verlernen.', 1, 16, 0, 0, 20002, 0, 0, 'Wollt Ihr wirklich die Talente eures Begleiters verlernen?', 14, 0, 4, 19, 0, 0, 0, 0, 0);
+(4783, 0, 0, 'Ich m�chte die Talente meines Beglieters verlernen.', 1, 16, 0, 0, 20002, 0, 0, 'Wollt Ihr wirklich die Talente eures Begleiters verlernen?', 14, 0, 4, 19, 0, 0, 0, 0, 0);
--- sql/custom/076_01_mangos_spell_bonus_data(OV60).sql
+++ sql/custom/076_01_mangos_spell_bonus_data(OV60).sql
@@ -1,3 +1,3 @@
-﻿-- Flametongue Weapon Attack; woraround sp scaling is hardcoded in the core
+-- Flametongue Weapon Attack; woraround sp scaling is hardcoded in the core
 DELETE FROM `spell_bonus_data` WHERE `entry` = 10444;
 INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `comments`) VALUES ('10444', '0', '0', '0', 'Shaman - Flametongue Attack (proc)');
