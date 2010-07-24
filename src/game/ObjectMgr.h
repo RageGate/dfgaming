703e843d11c2798732e9903d3a8491956e40af3c
VladimirMangos <vladimir@getmangos.com>
2010-07-25 00:00:52 +0400

    [10266] Implement CONDITION_ACHIEVEMENT and CONDITION_ACHIEVEMENT_REALM

--- src/game/ObjectMgr.h
+++ src/game/ObjectMgr.h
@@ -372,10 +372,12 @@ enum ConditionType
     CONDITION_SPELL                 = 17,                   // spell_id     0, 1 (0: has spell, 1: hasn't spell)
     CONDITION_INSTANCE_SCRIPT       = 18,                   // map_id       instance_condition_id (instance script specific enum)
     CONDITION_QUESTAVAILABLE        = 19,                   // quest_id     0       for case when loot/gossip possible only if player can start quest
-    CONDITION_PET_ACTIVE_NOT_DEAD   = 20,                   // 0            0
+    CONDITION_ACHIEVEMENT           = 20,                   // ach_id       0, 1 (0: has achievement, 1: hasn't achievement) for player
+    CONDITION_ACHIEVEMENT_REALM     = 21,                   // ach_id       0, 1 (0: has achievement, 1: hasn't achievement) for server
+    CONDITION_PET_ACTIVE_NOT_DEAD   = 22,                   // 0            0
 };
 
-#define MAX_CONDITION                 21                    // maximum value in ConditionType enum
+#define MAX_CONDITION                 23                    // maximum value in ConditionType enum
 
 struct PlayerCondition
 {
