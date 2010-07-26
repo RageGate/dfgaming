129c02e5cb70cdc7c7017e14f04d5836dc8365b2
Lynx3d <lynx3d@some-imaginary-isp.org>
2010-07-26 08:08:36 +0200

    [10272] Add option for search distance to getHeight() functions and make Creature::FallGround() use VMaps properly.
    This finally prevents flying creatures from falling to infinity (basically instantly diappearing) in several instances,
    aswell as prevent creatures from falling inside larger solid object around the world.
    
    Default height search is untouched, needs more research on how creature AI etc. will be affected.

--- src/game/Creature.cpp
+++ src/game/Creature.cpp
@@ -1379,8 +1379,8 @@ bool Creature::FallGround()
     if (getDeathState() == DEAD_FALLING)
         return false;
 
-    // Let's do with no vmap because no way to get far distance with vmap high call
-    float tz = GetMap()->GetHeight(GetPositionX(), GetPositionY(), GetPositionZ(), false);
+    // use larger distance for vmap height search than in most other cases
+    float tz = GetMap()->GetHeight(GetPositionX(), GetPositionY(), GetPositionZ(), true, MAX_FALL_DISTANCE);
 
     // Abort too if the ground is very near
     if (fabs(GetPositionZ() - tz) < 0.1f)
@@ -1819,7 +1819,7 @@ bool Creature::LoadCreaturesAddon(bool reload)
             }
 
             SpellAuraHolder *holder = GetSpellAuraHolder(cAura->spell_id, GetGUID());
-                   
+
             bool addedToExisting = true;
             if (!holder)
             {
@@ -1838,7 +1838,7 @@ bool Creature::LoadCreaturesAddon(bool reload)
             }
             else
                 AddSpellAuraHolder(holder);
-         
+
             DEBUG_FILTER_LOG(LOG_FILTER_SPELL_CAST, "Spell: %u - Aura %u added to creature (GUIDLow: %u Entry: %u )", cAura->spell_id, AdditionalSpellInfo->EffectApplyAuraName[EFFECT_INDEX_0],GetGUIDLow(),GetEntry());
         }
     }
